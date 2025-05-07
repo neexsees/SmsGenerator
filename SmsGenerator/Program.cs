891a72cf79105a898a23d82ce034a08e334b9dbc
Mariyna Veronika <vn1159@mail.ru>
2025-05-07 19:18:01 +0500

    feat: The ReadInput function has been created for user input

--- SmsGenerator/Program.cs
+++ SmsGenerator/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace SmsGenerator
+﻿using System;
+
+namespace SmsGenerator
 {
     class Program
     {
+        //функция для проверки пользовательского ввода
+        static int ReadInput(string request)
+        {
+            Console.WriteLine(request);
+            int validInput;
+            while (!int.TryParse(Console.ReadLine(), out validInput))
+            {
+                Console.WriteLine("Неверный ввод! Введите целое положительное число!");
+            }
+            return validInput;
+        }
+
         static void Main(string[] args)
         {
-            //Главный блок
+            int codeLength = ReadInput("Какой длины создать код?");
+            int codeNumber = ReadInput("Сколько кодов создать?");
         }
     }
 }
