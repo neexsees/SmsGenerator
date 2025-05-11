312eff570e23cffbab9209fa0b517f3a68879f3f
Mariyna Veronika <vn1159@mail.ru>
2025-05-11 17:08:46 +0500

    The Euclidean algorithm is implemented to check for the mutual simplicity of a number.

--- SmsGenerator/Program.cs
+++ SmsGenerator/Program.cs
@@ -1,25 +1,53 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SmsGenerator
 {
     class Program
     {
+        static Random rnd = new Random();
+
         //функция для проверки пользовательского ввода
         static int ReadInput(string request)
         {
             Console.WriteLine(request);
             int validInput;
-            while (!int.TryParse(Console.ReadLine(), out validInput))
+            while (!int.TryParse(Console.ReadLine(), out validInput) || validInput <=0)
             {
                 Console.WriteLine("Неверный ввод! Введите целое положительное число!");
             }
+
             return validInput;
         }
 
+        //Расчет остатка от деления
+        static int EuclidsAlgorithm(int a, int n)
+        {
+            while (n != 0)
+            {
+                int r = a % n;
+                a = n;
+                n = r;
+            }
+
+            return a;
+        }
         static void Main(string[] args)
         {
-            int codeLength = ReadInput("Какой длины создать код?");
-            int codeNumber = ReadInput("Сколько кодов создать?");
+            //Рандомная генерация взаимнопростых чисел
+            int a, n;
+            do
+            {
+                a = rnd.Next(1, 100);
+                n = rnd.Next(1, 100);
+            } while ((EuclidsAlgorithm(a,n) != 1)); //пока не станет равным 1, то есть не станут взаимно простыми
+
+            //Только для разработчиков(строку не должен видеть пользователь)
+            Console.WriteLine($"a = {a}, n = {n} — взаимно простые!");
+
+
+            int codeLength = ReadInput("Введите длину одного кода (в цифрах):");
+            int codeNumber = ReadInput("Сколько кодов сгенерировать?");
         }
     }
 }
